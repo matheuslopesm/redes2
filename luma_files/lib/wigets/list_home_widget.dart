d5df6f2ad804eb98868d3851f14f821a40c92515
Luisa Brito <lulubritodias@gmail.com>
2025-03-18 19:40:19 -0300

    fix: conserta widget da lista

--- luma_files/lib/wigets/list_home_widget.dart
+++ luma_files/lib/wigets/list_home_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:luma_files/utils/utils.dart';
 
 class ListHomeWidget extends StatelessWidget {
   const ListHomeWidget({
@@ -11,65 +12,79 @@ class ListHomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-          child: Container(
-            height: 40,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.white,
-            ),
-            child: Center(
-              child: Text(
-                fileName,
-                style: GoogleFonts.inter(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
+    return Container(
+      height: 76,
+      decoration: BoxDecoration(
+        color: LumaFilesConstants.secondaryColor,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Text(
+                    fileName,
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-        const SizedBox(width: 8),
-        Container(
-          height: 40,
-          width: 40,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: Colors.white,
-          ),
-          child: Center(
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.download,
-                color: Colors.black,
+            const SizedBox(width: 8),
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: Center(
+                child: IconButton(
+                  onPressed: () {
+                    // vou ter uma função que vai baixar o arquivo
+                  },
+                  icon: const Icon(
+                    Icons.download,
+                    color: Colors.black,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        const SizedBox(width: 8),
-        Container(
-          height: 40,
-          width: 40,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: Colors.white,
-          ),
-          child: Center(
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.black,
+            const SizedBox(width: 8),
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: Center(
+                child: IconButton(
+                  onPressed: () {
+                    // vou ter uma função que vai deletar o arquivo
+                  },
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                  ),
+                ),
               ),
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
